3fcba09c93821315925b19d59ab5c311f8fd7dcb
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-09-07 14:59:26 +0200

    added uploadcontroller

---
+++ src/controller/UploadController.ts
@@ -0,0 +1,47 @@
+import { NextFunction, Request, Response } from "express";
+import * as Loki from "lokijs";
+
+// setup
+const DB_NAME = "db.json";
+const COLLECTION_NAME = "images";
+const UPLOAD_PATH = "uploads";
+// const upload = multer({ dest: `${UPLOAD_PATH}/` }); // multer configuration
+const db = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, {
+  persistenceMethod: "fs"
+});
+
+const loadCollection = function(
+  colName,
+  db: Loki
+): Promise<Loki.Collection<any>> {
+  return new Promise(resolve => {
+    db.loadDatabase({}, () => {
+      const _collection =
+        db.getCollection(colName) || db.addCollection(colName);
+      resolve(_collection);
+    });
+  });
+};
+
+export class UploadController {
+  async save(request: Request, response: Response, next: NextFunction) {
+    try {
+      console.log("YES");
+      console.log(request.headers["content-type"]);
+      const col = await loadCollection(COLLECTION_NAME, db);
+      const data = col.insert(request.file);
+
+      db.saveDatabase();
+
+      let result = {
+        id: data.$loki,
+        fileName: data.filename,
+        originalName: data.originalname
+      };
+
+      return result;
+    } catch (err) {
+      response.sendStatus(400);
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
+import * as bodyParser from "body-parser";
+import * as express from "express";
+import { Request, Response } from "express";
+import * as multer from "multer";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
 import { Routes } from "./routes";
-import { User } from "./entity/User";
-import * as request from "request-promise-native";
-import * as multer from "multer";
-import * as fs from "fs";
-import * as path from "path";
-import * as Loki from "lokijs";
-import * as del from "del";
 
 createConnection()
   .then(async connection => {
@@ -18,89 +12,74 @@ createConnection()
     const app = express();
     app.use(bodyParser.json());
 
+    const UPLOAD_PATH = "uploads";
+    const upload = multer({ dest: `${UPLOAD_PATH}/` });
+
     // register express routes from defined application routes
     Routes.forEach(route => {
-      (app as any)[route.method](
-        route.route,
-        (req: Request, res: Response, next: Function) => {
-          const result = new (route.controller as any)()[route.action](
-            req,
-            res,
-            next
-          );
-          if (result instanceof Promise) {
-            result.then(
-              result =>
-                result !== null && result !== undefined
-                  ? res.send(result)
-                  : undefined
+      if (route.route == "/upload") {
+        (app as any)[route.method](
+          route.route,
+          upload.single("pic"),
+          (request: Request, res: Response, next: Function) => {
+            const result = new (route.controller as any)()[route.action](
+              request,
+              res,
+              next
             );
-          } else if (result !== null && result !== undefined) {
-            res.json(result);
+            if (result instanceof Promise) {
+              result.then(
+                result =>
+                  result !== null && result !== undefined
+                    ? res.send(result)
+                    : undefined
+              );
+            } else if (result !== null && result !== undefined) {
+              res.json(result);
+            }
           }
-        }
-      );
-    });
-
-    // setup
-    const DB_NAME = "db.json";
-    const COLLECTION_NAME = "images";
-    const UPLOAD_PATH = "uploads";
-    const upload = multer({ dest: `${UPLOAD_PATH}/` }); // multer configuration
-    const db = new Loki(`${UPLOAD_PATH}/${DB_NAME}`, {
-      persistenceMethod: "fs"
-    });
-
-    const loadCollection = function(
-      colName,
-      db: Loki
-    ): Promise<Loki.Collection<any>> {
-      return new Promise(resolve => {
-        db.loadDatabase({}, () => {
-          const _collection =
-            db.getCollection(colName) || db.addCollection(colName);
-          resolve(_collection);
-        });
-      });
-    };
-
-    app.post("/anal", upload.single("pic"), async (req, res) => {
-      try {
-        const col = await loadCollection(COLLECTION_NAME, db);
-        const data = col.insert(req.file);
-
-        db.saveDatabase();
-        res.send({
-          id: data.$loki,
-          fileName: data.filename,
-          originalName: data.originalname
-        });
-      } catch (err) {
-        res.sendStatus(400);
+        );
+      } else {
+        // const uploadCheck = route.route == "/upload" ? upload.single("pic") : (() => {});
+        (app as any)[route.method](
+          route.route,
+          // uploadCheck,
+          (req: Request, res: Response, next: Function) => {
+            const result = new (route.controller as any)()[route.action](
+              req,
+              res,
+              next
+            );
+            if (result instanceof Promise) {
+              result.then(
+                result =>
+                  result !== null && result !== undefined
+                    ? res.send(result)
+                    : undefined
+              );
+            } else if (result !== null && result !== undefined) {
+              res.json(result);
+            }
+          }
+        );
       }
     });
+
+    function checkMultipart(req, res, next) {
+      const contentType = req.headers["content-type"];
+      // Make sure it's multipart/form
+      if (!contentType || !contentType.includes("multipart/form-data")) {
+        // Stop middleware chain and send a status
+        return res.sendStatus(500);
+      }
+      next();
+    }
 
     // setup express app here
     // ...
 
     // start express server
     app.listen(3000);
-
-    // insert new users for test
-    // await connection.manager.save(
-    //   connection.manager.create(User, {
-    //     firstName: "Timber",
-    //     lastName: "Saw",
-    //     age: 27
-    //   })
-    // );
-    // await connection.manager.save(
-    //   connection.manager.create(User, {
-    //     firstName: "Phantom",
-    //     lastName: "Assassin",
-    //     age: 24
-    //   })
-    // );
 
     console.log(
       "Express server has started on port 3000. Open http://localhost:3000/users to see results"
