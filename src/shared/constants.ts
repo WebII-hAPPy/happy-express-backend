90c5ae8d2306fb1ff5a74c4cb3a6de6a1f7da9d5
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-09-12 10:12:05 +0200

    moved constants

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -6,4 +6,10 @@ const URI_BASE = "https://westeurope.api.cognitive.microsoft.com";
 
 const OCP_APIM_SUBSCRIPTION_KEY = "2f43ea3609644ba99c3a8cdae4382178";
 
-export { DB_NAME, COLLECTION_NAME, UPLOAD_PATH, URI_BASE };
+export {
+  DB_NAME,
+  COLLECTION_NAME,
+  UPLOAD_PATH,
+  URI_BASE,
+  OCP_APIM_SUBSCRIPTION_KEY
+};
