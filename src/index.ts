702735ed50ad65ddca45543261cea9a118099876
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-09-15 18:14:34 +0200

    reformatted code

--- src/index.ts
+++ src/index.ts
@@ -79,8 +79,6 @@ createConnection()
     // start express server
     app.listen(3000);
 
-    console.log(
-      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
-    );
+    console.log("Express server has started on port 3000.");
   })
   .catch((error) => console.log(error));
