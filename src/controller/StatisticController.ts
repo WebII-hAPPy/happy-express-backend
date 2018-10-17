2985fbe6d66ebb7c135657209d186e48d6442fec
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-10-17 16:40:00 +0200

    added statistic reset

--- src/controller/StatisticController.ts
+++ src/controller/StatisticController.ts
@@ -5,10 +5,12 @@ import { User } from "../entity/User";
 import { AuthService } from "../services/auth.service";
 import { IResponse } from "../models/Response.model";
 import { IEmotionWithTimestamp } from "../models/EmotionsWithTimestamp";
+import { AnalysisController } from "./AnalysisController";
 
 export class StatisticController {
   authService: AuthService;
   private userRepository = getRepository(User, process.env.NODE_ENV);
+  private analysisRepository = getRepository(Analysis, process.env.NODE_ENV);
 
   constructor() {
     this.authService = new AuthService();
@@ -58,6 +60,35 @@ export class StatisticController {
         status: 200,
         message: `Request granted. Returning statistics for user ${user.id}`,
         data: emotions
+      };
+    }
+    return {
+      status: 406,
+      message: "Could not affirm identity"
+    };
+  }
+
+  public async reset(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ): Promise<IResponse> {
+    if (this.authService.affirmIdentity(request)) {
+      const user: User = await this.userRepository
+        .createQueryBuilder("user")
+        .where("user.id = :id", { id: request.params.id })
+        .select()
+        .leftJoinAndSelect("user.analyses", "analysis")
+        .innerJoinAndSelect("analysis.emotion", "emotion")
+        .getOne();
+
+      user.analyses.forEach(async (analysis: Analysis) => {
+        await this.analysisRepository.remove(analysis);
+      });
+
+      return {
+        status: 200,
+        message: "Successfully removed statistics"
       };
     }
     return {
