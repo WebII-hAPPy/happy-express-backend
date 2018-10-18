0a2c6cd1f991243e1eaa8c60e33f640ef8352b02
constantin <constantin.ruhdorfer@gmx.de>
2018-10-18 17:45:02 +0200

    Added unittest for /login route

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -34,7 +34,7 @@ export class AuthController {
     if (!(request.body && request.body.email && request.body.password)) {
       return {
         status: 401,
-        message: "Username and password are required."
+        message: "Email and password are required."
       };
     }
 
